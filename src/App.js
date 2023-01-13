3f924a633886eb879490654c820f0eb065ba1034
Bney28 <bney1@outlook.com>
2023-01-13 16:18:09 -0500

    feat: Updated Imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import NavBar from "./Components/header";
 import Card from "./Components/cards"
 import CardDesktop from "./Components/cardsDesktop";
-import "./Styles/style-header.css"
 import Hero from "./Components/hero";
+import Footer from "./Components/footer"
+import Interactive from "./Components/interactive"
+import "./Styles/style-header.css"
+import "./Styles/our-creations.css"
+import "./Styles/style-interactive-footer.css"
 
 
 function App() {
   return (
     <>
-    <NavBar />
-    <Hero />
-      {/* <NavBar /> */}
-    <Card />
-    <CardDesktop />
+      <NavBar />
+      <Hero />
+      <Card />
+      <CardDesktop />
       <Interactive />
       <Footer />
     </>
