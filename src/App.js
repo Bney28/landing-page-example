a2e975b5a9805680f635b54d31da3ad22d5afbce
moralesvictorr <vmmorales@unicauca.edu.co>
2023-01-14 16:18:53 -0500

    feat: changed to SASS

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import CardDesktop from "./Components/cardsDesktop";
 import Hero from "./Components/hero";
 import Footer from "./Components/footer"
 import Interactive from "./Components/interactive"
-import "./Styles/style-header.css"
-import "./Styles/our-creations.css"
-import "./Styles/style-interactive-footer.css"
+import "./Styles/style-header.scss"
+import "./Styles/our-creations.scss"
+import "./Styles/style-interactive-footer.scss"
 
 
 function App() {
